217bfd99c494826b141074bfa033f5b0196360b0
haxwithaxe <spam@haxwithaxe.net>
2022-06-28 21:28:38 -0400

    Reconfigured metadata

--- setup.py
+++ setup.py
@@ -1,15 +1,6 @@
 #!/usr/bin/env python3
 
-from distutils.core import setup
+from setuptools import setup
 
-setup(
-    name='ntfyr',
-    version='0.1',
-    description='A simple client for ntfy.sh.',
-    author='haxwithaxe',
-    author_email='spam@haxwithaxe.net',
-    url='https://github.com/haxwithaxe/ntfyr',
-    packages=['ntfyr'],
-    license='License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-    scripts=['scripts/ntfyr'],
-)
+if __name__ == '__main__':
+    setup()
